8c9fc0594819fc8bbe285b9eb4ac34e646c1279b
thguss <thgus345@naver.com>
2023-02-02 01:56:39 +0900

    [INIT] 프로젝트 초기 세팅

---
+++ src/test/java/com/lecture/orm/JavaOrmJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lecture.orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOrmJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
