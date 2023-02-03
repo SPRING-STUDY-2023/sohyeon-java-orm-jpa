29e0aec7f3c9277b136ce2305ff62663ab035326
thguss <thgus345@naver.com>
2023-02-04 02:28:48 +0900

    영속성 컨텍스트

--- src/main/java/com/lecture/orm/JavaOrmJpaApplication.java
+++ src/main/java/com/lecture/orm/JavaOrmJpaApplication.java
@@ -16,24 +16,39 @@ public class JavaOrmJpaApplication {
         ts.begin();
 
         try {
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("A");
+            // 비영속
+            Member member1 = new Member(1L, "A");
+            Member member2 = new Member(2L, "B");
 
-            em.persist(member); // create
+            // create, 영속
+            em.persist(member1);
+            em.persist(member2);
 
-            Member foundMember = em.find(Member.class, 1L); // read
+            // 준영속 상태로 변경 (JPA 에서 관리하지 않는 상태)
+            em.detach(member2);
+//            em.clear(); // 영속성 컨텍스트 완전 초기화
+//            em.close(); // 영속성 컨텍스트 종료
 
-            foundMember.setName("B"); // update
+            // 플러시 강제 호출 (쿼리 실행)
+            em.flush();
 
-            em.remove(foundMember); // delete
+            // read
+            Member foundMember = em.find(Member.class, 1L);
 
+            // update (변경 감지)
+            foundMember.setName("C");
+
+            // delete
+            em.remove(foundMember);
+
+            // JPQL 실행 (실행 시 강제 플러시)
             // 쿼리 조회 -> "객체"로 조회 : List<Member>
             em.createQuery("select m from Member as m", Member.class)
                     .setFirstResult(5)
                     .setMaxResults(8)
                     .getResultList();
 
+            // 쿼리 실행 (쓰기 지연)
             ts.commit();
         } catch (Exception e) {
             ts.rollback();
