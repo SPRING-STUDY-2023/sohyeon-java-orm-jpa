e1ba86170184c7f83ee36b224aee8dc4249e0c04
thguss <thgus345@naver.com>
2023-02-05 20:03:26 +0900

    양방향 연관관계와 연관관계의 주인

--- src/main/java/com/lecture/orm/section5/Member.java
+++ src/main/java/com/lecture/orm/section5/Member.java
@@ -15,4 +15,29 @@ public class Member {
     @ManyToOne
     @JoinColumn(name = "TEAM_ID")
     private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void changeTeam(Team team) { // 연관관계 편의 메소드
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }
