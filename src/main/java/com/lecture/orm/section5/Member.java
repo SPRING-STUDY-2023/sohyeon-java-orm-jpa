7b0f93e61347cd0a3ae44ff4fd3d46e3aeedf31e
thguss <thgus345@naver.com>
2023-02-05 18:39:24 +0900

    단방향 연관관계

---
+++ src/main/java/com/lecture/orm/section5/Member.java
@@ -0,0 +1,18 @@
+package com.lecture.orm.section5;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+}
---
+++ src/main/java/com/lecture/orm/section5/Team.java
@@ -0,0 +1,16 @@
+package com.lecture.orm.section5;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "TEAM_ID")
+    private Long id;
+
+    private String name;
+}
