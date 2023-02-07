a8393919c8e3f9b077d63c111b4e25c069816538
thguss <thgus345@naver.com>
2023-02-07 21:38:19 +0900

    매핑 정보 상속

--- src/main/java/com/lecture/orm/section7/Item.java
+++ src/main/java/com/lecture/orm/section7/Item.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
-public class Item {
+public class Item extends BaseEntity { // 공통 Entity 상속
 
     @Id @GeneratedValue
     private Long id;
