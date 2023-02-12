3a9b9f3544e02083afda635cd2ed97bf07bcf9c7
thguss <thgus345@naver.com>
2023-02-12 23:41:11 +0900

    [3주차] 값 타입

---
+++ src/main/java/com/lecture/orm/section9/Member.java
@@ -0,0 +1,30 @@
+package com.lecture.orm.section9;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    // 기간 Period
+    @Embedded
+    private Period workPeriod;
+
+    // 주소 Address
+    @Embedded
+    private Address homeAddress;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city", column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street", column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipcode", column = @Column(name = "WORK_ZIPCODE"))
+    })
+    private Address workAddress;
+}
